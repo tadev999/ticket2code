c018edfd4d520d4ac06bf4fe536bd469287134ac
TADEV <tadev999@gmail.com>
2026-06-30 16:34:00 +0900

    Delete core directory

--- core/skills/figma-svg-export/scripts/figma_export_svg.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-set "SCRIPT_DIR=%~dp0"
-if exist "%ProgramFiles%\PowerShell\7\pwsh.exe" (
-  "%ProgramFiles%\PowerShell\7\pwsh.exe" -NoProfile -File "%SCRIPT_DIR%figma_export_svg.ps1" %*
-  exit /b %ERRORLEVEL%
-)
-powershell -NoProfile -File "%SCRIPT_DIR%figma_export_svg.ps1" %*
-exit /b %ERRORLEVEL%
